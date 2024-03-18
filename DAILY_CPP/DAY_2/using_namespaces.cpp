c6796573d62b6967c04cd67e48d8c1f9849c4333
satejg31 <satej.ganji@gmail.com>
2024-03-18 23:07:24 +0530

    Initial commit

---
+++ DAILY_CPP/DAY_2/using_namespaces.cpp
@@ -0,0 +1,22 @@
+/*
+ * Program which simulates the usage of namespaces in the CPP Programs
+ *
+ */
+
+#include <iostream>
+
+using std::cin;
+using std::cout;
+using std::string;
+using std::endl;
+
+int main(void)
+{
+	cout << "Enter a string litreal : ";
+	string s;
+	cin >> s;
+	cout << s << endl;
+
+	return (0);
+}
+
